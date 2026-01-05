91a58bdb046f2036d5119f912783e3df3cfa2bec
Pranav <pranavsao50@gmail.com>
2026-01-05 19:57:32 +0530

    NA
    BA

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -24,7 +24,7 @@ export function Navbar() {
             to="/" 
             className="flex items-center"
           >
-            <img src={logo} alt="The Practical World" className="h-8" />
+            <img src={logo} alt="The Practical World" className="h-10" />
           </Link>
 
           {/* Desktop Navigation */}
