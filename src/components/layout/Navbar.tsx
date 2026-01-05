9c7aaa164cfd8b0c3b4331f64ce5ca68ad039a9a
Pranav <pranavsao50@gmail.com>
2026-01-05 20:07:48 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -24,7 +24,7 @@ export function Navbar() {
             to="/" 
             className="flex items-center"
           >
-            <img src={logo} alt="The Practical World" className="h-10" />
+            <img src={logo} alt="The Practical World" className="h-15" />
           </Link>
 
           {/* Desktop Navigation */}
